80346c19131f6b0fc0a36025548bb49972d4ea61
nelsonic <nelson+github@dwyl.io>
2020-03-31 18:56:32 +0100

    IO.inspect response

--- test/app_web/controllers/invoke_lambda_test.exs
+++ test/app_web/controllers/invoke_lambda_test.exs
@@ -6,11 +6,11 @@ defmodule AppWeb.InvokeLambdaControllerTest do
       name: "Elixir Lover",
       email: System.get_env("RECIPIENT_EMAIL_ADDRESS"),
       template: "welcome",
-      id: "1"
+      id: "145"
     }
 
     {:ok, response} = AppWeb.InvokeLambdaController.invoke(payload)
-    # IO.inspect(response, label: "response")
+    IO.inspect(response, label: "response")
     message_id = Map.get(response, "message_id")
     assert String.length(message_id) == 60
   end
